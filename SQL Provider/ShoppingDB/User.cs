dd59bcb542e5fec4094cdee3d673d044536c91cd
mohamed.mansour <mohamed.mansour@tribal.credit>
2022-06-02 17:01:11 +0200

    Create JWT Authentication

---
+++ SQL Provider/ShoppingDB/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Provider.ShoppingDB
+{
+    public class User
+    { 
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ Shopping Project/Controllers/AuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Entites.Parameters;
+using Business.Processors;
+using Microsoft.AspNetCore.Mvc;
+using SQL_Provider.ShoppingDB;
+
+namespace Shopping_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private AuthenticationProcessor _authenticationProcessor = null;
+        public static User user = new User();
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            _authenticationProcessor = new AuthenticationProcessor(_configuration);
+            _authenticationProcessor.CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.UserName = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Ok(user);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            _authenticationProcessor = new AuthenticationProcessor(_configuration);
+
+            if (request.Username != user.UserName)
+                return BadRequest("Wrong username");
+
+            if(!_authenticationProcessor.VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                return BadRequest("Wrong Password");
+
+            var token = _authenticationProcessor.CreateToken(user);
+            return Ok(token);
+        }
+
+
+
+    }
+}
